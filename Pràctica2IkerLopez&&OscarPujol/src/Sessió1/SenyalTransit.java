c0f0f546e0bca79c2f95a543efd36754dcf7a2d3
OscarPujol15 <opujolr@edu.tecnocampus.cat>
2025-01-31 11:57:29 +0100

    Update SenyalTransit.java

--- Pràctica2IkerLopez&&OscarPujol/src/Sessió1/SenyalTransit.java
+++ Pràctica2IkerLopez&&OscarPujol/src/Sessió1/SenyalTransit.java
@@ -1,5 +1,38 @@
 package Sessió1;
 
 public class SenyalTransit {
+  	private Codi codi;
+	private Ubicacio ubicacio;
+	private int anyUbicacio;
+
+	public SenyalTransit(Codi codi, Ubicacio ubicacio, int anyColocacio){
+		this.codi = codi;
+		if (!afegirSenyal(SenyalTransit)){
+			this.ubicacio = null;
+		}
+		else {
+			this.ubicacio = ubicacio;
+		}
+		this.anyUbicacio = anyColocacio;
+
+	}
+
+	public String donaTipusSenyal(){
+		return codi.getTipus();
+	}
+
+	public boolean retirarViaPublica(){
+		if (!ubicacio.treureSenyal(SenyalTransit)){
+		}
+		else {
+			setUbicacio(null);
+			if (this.anyubicacio>0){
+				anyubicacio = 0;
+			}
+			else{
+			}
+		}
+	}
+}
 
 }
