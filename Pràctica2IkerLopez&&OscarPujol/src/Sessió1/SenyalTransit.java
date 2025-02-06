e430987a843c4b0e29a1253161c9898b4ef2bac3
OscarPujol15 <opujolr@edu.tecnocampus.cat>
2025-02-06 23:05:46 +0100

    Update SenyalTransit.java

--- Pràctica2IkerLopez&&OscarPujol/src/Sessió1/SenyalTransit.java
+++ Pràctica2IkerLopez&&OscarPujol/src/Sessió1/SenyalTransit.java
@@ -1,10 +1,10 @@
 package Sessió1;
 
 public class SenyalTransit {
-  	private Codi codi;
+	private Codi codi;
 	private Ubicacio ubicacio;
 	private int anyUbicacio;
-
+	
 	public SenyalTransit(Codi codi, Ubicacio ubicacio, int anyColocacio){
 		this.codi = codi;
 		if (!afegirSenyal(SenyalTransit)){
@@ -14,25 +14,37 @@ public class SenyalTransit {
 			this.ubicacio = ubicacio;
 		}
 		this.anyUbicacio = anyColocacio;
-
 	}
-
+	
 	public String donaTipusSenyal(){
 		return codi.getTipus();
 	}
-
+	
 	public boolean retirarViaPublica(){
-		if (!ubicacio.treureSenyal(SenyalTransit)){
-		}
-		else {
-			setUbicacio(null);
-			if (this.anyubicacio>0){
-				anyubicacio = 0;
-			}
-			else{
+		if (ubicacio.treureSenyal(SenyalTransit)){
+		setUbicacio(null);
+			if (this.anyubicacio!=0){
+				anyUbicacio = 0;
 			}
 		}
 	}
-}
-
+	
+	public String getUbicacio(){ 
+		if (this.ubicacio != null){
+		return ubicacio. + ubicacio.}
+		else{
+		return null;
+		}
+		// Preguntar
+	}
+	
+	public boolean canviarUbicacio (Ubicacio novaUbicacio){
+		if(ubicacio.afegirSenyal(SenyalTransit, int)){
+			ubicacio.treureSenyal(SenyalTransit);
+			this.ubicacio = novaUbicacio;
+			return true; //Preguntar
+		}
+	}
+	
+	public Codi getCodi(){return getIdentificador();}
 }
